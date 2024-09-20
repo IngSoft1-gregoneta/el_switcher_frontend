2ff2f136dc77144f022816a4d01f2124f440d6bb
Gregorio Vilardo <gregovilardo@gmail.com>
2024-09-20 17:39:28 -0300

    SES-9 renaming old lobbys to room

--- src/containers/App/components/AppLayout.jsx
+++ src/containers/App/components/AppLayout.jsx
@@ -22,7 +22,7 @@ export default function AppLayout({ lastMessage, createRoom }) {
           className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-[active]:bg-sky-700 data-[hover]:bg-sky-500"
           name="add_game"
         >
-          Create Game
+          Create Room
         </Button>
       </Link>
     </div>
--- src/containers/Room/Room.test.jsx
+++ src/containers/Room/Room.test.jsx
@@ -205,4 +205,3 @@ describe("Room tests", () => {
     cleanup();
   });
 });
-
