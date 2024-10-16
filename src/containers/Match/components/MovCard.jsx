ebd9e300e626e19bedc5905afdcb0ed882494a8c
nico_nielsen <niconielsen.pc@gmail.com>
2024-10-16 19:14:40 -0300

    SES-104 desactivar carta usada, revertir movimiento

--- src/containers/Match/components/MovCard.jsx
+++ src/containers/Match/components/MovCard.jsx
@@ -3,22 +3,33 @@ import images from "../logic/bindImage";
 import { useMovCardStore } from "../../../zustand/store";
 
 export default function MovCard({ card, index }) {
-    const selectedMovCard = useMovCardStore((state) => state.selectedMovCard);
-    const setSelectedMovCard = useMovCardStore((state) => state.setSelectedMovCard);
+  const selectedMovCard = useMovCardStore((state) => state.selectedMovCard);
+  const setSelectedMovCard = useMovCardStore(
+    (state) => state.setSelectedMovCard
+  );
 
-    const handleClick = () => {
-        const new_card = { ...card, index };
-        if (new_card == selectedMovCard) selectedMovCard(null);
-        setSelectedMovCard(new_card);
+  const handleClick = () => {
+    const new_card = { ...card, index };
+
+    if (selectedMovCard && new_card.index === selectedMovCard.index) {
+      setSelectedMovCard(null);
+    } else if(card.is_used == true){
+        setSelectedMovCard(null);
+    } else {
+      setSelectedMovCard(new_card);
     }
+  };
 
-    return (
-        <img
-            onClick={handleClick}
-            src={images[`${card.mov_type}`]}
-            key={index}
-            data-testid="me-mov-cards"
-            className="mov_card aspect-[3/5] h-12 rounded-sm md:h-32 lg:h-36"
-        />
-    );
-};
+  return (
+    <img
+      onClick={handleClick}
+      src={images[`${card.mov_type}`]}
+      key={index}
+      data-testid="me-mov-cards"
+      className={`mov_card aspect-[3/5] h-12 rounded-sm md:h-32 lg:h-36
+        ${card.is_used ? "unavailable" : ""}
+      `}
+      title={card.is_used ? "Esta carta ya ha sido usada" : ""}
+    />
+  );
+}
